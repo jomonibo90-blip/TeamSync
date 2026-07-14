794c6c864fbc937cb7c33b85f088b7b04f95cfe5
ramankumaree202-collab <ramankumaree202@gmail.com>
2026-07-14 12:37:54 -0400

    chore(migrations): add AddTaskStartDate migration

--- TeamSync/TeamSync/Migrations/20260714163740_AddTaskStartDate.Designer.cs
+++ TeamSync/TeamSync/Migrations/20260714163740_AddTaskStartDate.Designer.cs
@@ -3,17 +3,20 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TeamSync.Data;
 
 #nullable disable
 
-namespace TeamSync.Migrations
+namespace TeamSync.TeamSync.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260714163740_AddTaskStartDate")]
+    partial class AddTaskStartDate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ TeamSync/TeamSync/Migrations/20260714163740_AddTaskStartDate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TeamSync.TeamSync.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTaskStartDate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "StartDate",
+                table: "Tasks",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "StartDate",
+                table: "Tasks");
+        }
+    }
+}
